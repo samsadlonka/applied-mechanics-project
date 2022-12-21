d32d743f0cf4115e090557da6ad5b038ff8e3de8
samsadlonka <lr24092003@yandex.ru>
2022-12-21 16:17:48 +0300

    add input_parameters.txt and shemi png

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from beam import Point, Beam
 
-L = int(input("Введите значение L для фермы"))
-h = int(input("Введите значение h для фермы"))
+with open("input_parameters.txt", 'rt', encoding='utf-8') as file:
+    lines = file.readlines()
+    L, h = list(map(lambda x: float(x.split(":")[1]), lines[:2]))
+    R, r = list(map(lambda x: float(x.split(":")[1]), lines[2:4]))
+    E, G = list(map(lambda x: float(x.split(":")[1]), lines[4:6]))
+    # список внешних сил
+    forces_list = list(map(lambda x: float(x.split(":")[1]), file.readlines()))
+
 with open("graph.txt", 'rt', encoding='utf-8') as file:
     # список смежностей
     adjacency_list = list(map(lambda x: list(map(int, x.split())), file.readlines()))
@@ -9,19 +15,13 @@ with open("graph.txt", 'rt', encoding='utf-8') as file:
     points += [Point(L / 6, 0, h / 3), Point(5 * L / 6, 0, h / 3)]
     points += [Point(L / 6 * i, 0, 0) for i in range(7)]
 
-with open("forces.txt", 'rt', encoding='utf-8') as file:
-    # список внешних сил
-    forces_list=list(map(lambda x: float(x.split(":")[1]), file.readlines()))
-
-
 list_of_beams = []
 
 for i in range(len(points)):
     neighbors = adjacency_list[i]
     for neighbor in neighbors:
-        new_beam = Beam(points[i], neighbor, 1, 1)
+        new_beam = Beam(points[i], neighbor, E, G, R, r)
         if new_beam not in list_of_beams:
             list_of_beams.append(new_beam)
 
 index_matrix = [[] for _ in range(5)] * 12
-
